2ffd211649bfaba8bcb3da76654b08d7c87d7877
shudj <ademin117603@163.com>
2018-11-28 14:51:20 +0800

    新建MessageObject

---
+++ src/main/java/com/jl/test/mode/producer2consumer/MessageObject.java
@@ -0,0 +1,10 @@
+package com.jl.test.mode.producer2consumer;
+
+/**
+ * Author: shudj
+ * Time: 2018/11/28 14:50
+ * Description:
+ */
+public class MessageObject {
+    public static String msg = "";
+}
