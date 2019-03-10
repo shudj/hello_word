11da5d722abf6cfe61faf106f4d0a3d1e37860cc
admin <admin117603@163.com>
2019-03-10 11:33:17 +0800

    zhuang shi mo shi

---
+++ tool/src/main/java/com/example/tool/mode/Decorate.java
@@ -0,0 +1,41 @@
+package com.example.tool.mode;
+
+/**
+ * zhuang shi she ji mo shi
+ * te dian:
+ *  ou he xing bu qiang, bei zhuang shi de lei de bian hua yu zhuang shi lei de bian hua wu guan
+ */
+public class Decorate {
+
+    public static void main(String[] args) {
+        new DecorateStu(new Student()).code();
+    }
+}
+
+
+interface Coder {
+    public void code();
+}
+
+class Student implements Coder {
+
+    @Override
+    public void code() {
+        System.out.println("java");
+    }
+}
+
+class DecorateStu implements Coder {
+
+    private Student stu;
+
+    public DecorateStu(Student stu) {
+        this.stu = stu;
+    }
+
+    @Override
+    public void code() {
+        stu.code();
+        System.out.println("decorate java");
+    }
+}
